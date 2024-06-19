7096b5a02ff47a5ad1f8f4f4a3215480dcc72998
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-19 13:28:11 -0300

    feat: add remove_first function declaration.

--- ordered_list.h
+++ ordered_list.h
@@ -20,5 +20,6 @@ void create_list(List *list);
 void insert_list(List *list, Node *node);
 void fill_list(unsigned int tab[], List *list);
 void print_list(List *list);
+Node * remove_first(List *list);
 
 #endif //ORDERED_LIST_H
