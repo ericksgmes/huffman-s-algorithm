259404a394e1d3ac73e41717e5865caea53df7d5
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-19 11:46:59 -0300

    fix: add comment and unsigned to function definition.

--- freq_table.h
+++ freq_table.h
@@ -4,10 +4,10 @@
 
 #ifndef FREQ_TABLE_H
 #define FREQ_TABLE_H
-#define TAM 256
+#define TAM 256 // tamanho da tabela ASCII
 
 void initialize_frequency_table(unsigned int tab[]);
 void fill_ft(unsigned char texto[], unsigned int tab[]);
-void imprime_tab_frequencia(unsigned int tab[]);
+void ft_print(unsigned int tab[]);
 
 #endif //FREQ_TABLE_H
