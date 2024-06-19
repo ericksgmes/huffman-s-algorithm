cda0d7751cbed187a25052b01b30e4d1343ac1a4
Erick Gomes <119770693+ericksgmes@users.noreply.github.com>
2024-06-19 12:03:59 -0300

    feat: implement insert_list function.

--- ordered_list.c
+++ ordered_list.c
@@ -10,3 +10,20 @@ void create_list(List *list) {
     list->first = NULL;
     list->size = 0;
 }
+
+void insert_list(List *list, Node *node) {
+    if (list->first == NULL) {
+        list->first = node;
+    }
+    else if (node->frequencia < list->first->frequencia) {
+        node->next = list->first;
+        list->first = node;
+    } else {
+        Node *aux = list->first;
+        while (aux->next && aux->next->frequencia <= node->frequencia)
+            aux = aux->next;
+        node->next = aux->next;
+        aux->next = node;
+    }
+    list->size++;
+}
